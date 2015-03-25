1ab87ed38645a3e56b439d8bb23276f55efd4191
Igor Lushchyk <ihor.lushchyk@sethq.com>
2015-03-25 21:48:09 +0200

    some shit

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+from os.path import isfile
+
+def read(path):
+    """Reads data in cron file"""
+    raw_f = open_path(path)
+    data = raw_f.readlines()
+    raw_f.close()
+    return data
+
+def write(path, data):
+    """Writes data in cron file"""
+    raw_f = open(path, 'w+')
+    raw_f.write(data + '\n')
+    raw_f.close()
+
