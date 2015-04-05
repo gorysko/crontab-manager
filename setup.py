7d81d560a1c819a7bd9fcc4371308ea7f30d58c4
Igor Lushchyk <ihor.lushchyk@sethq.com>
2015-04-05 20:29:02 +0300

    Added setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""Setup module."""
+from src.utils import read
+from setuptools import setup
+
+setup(
+    name='cron tab manager',
+    version='0.0.1',
+    author='Igor Lushchyk',
+    author_email='gorysko@gmail.com',
+    description=('Crontab manager'),
+    license='BSD',
+    keywords='',
+    url='',
+    packages=['src'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
