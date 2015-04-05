9b5cde77e78890de609a167e2dba40cc5771902d
Igor Lushchyk <ihor.lushchyk@sethq.com>
2015-04-05 20:58:23 +0300

    added command line interface

---
+++ src/command_line.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+"""Command line module"""
+
+from cron_manager import *
+
+def main():
+    """Parses command line args."""
+    usage = "usage: %prog [options] arg"
+    parser = OptionParser(usage)
+    parser.add_option('-c', '--crontab', dest='crontab',
+                      help='creates crontab')
+    parser.add_option('-j', '--cronjob', dest='cronjob',
+                      help='creates cronjob item')
+
+    parser.add_option('--list-crontabs', dest='lc',
+                      help='lists crontab')
+
+    parser.add_option('--list-cronjobs', dest='lj',
+                      help='lists cronjobs')
+
+    options, _ = parser.parse_args()
+
+
+    if options.crontab is not None:
+        add_cron(options.crontab)
+
+    if options.cronjob is not None:
+        cronjob = options.cronjob.split(',')
+        add_crontabitem(cronjob[0], cronjob[1], cronjob[2])
+
+    if options.lc is not None:
+        status = 0
+        if options.lc == 'active':
+            status = 1
+        print listing(status, 'cron')
+
+    if options.lj is not None:
+        status = 0
+        if options.lj == 'active':
+            status = 1
+        print listing(status, 'jobs')
+
+    if options.cronjob is not None and options.crontab is not None:
+        cron = add_cron(options.crontab)
+        cronjob = options.cronjob.split(',')
+        cron_item = add_crontabitem(cronjob[0], cronjob[1], cronjob[2])
+        link(cron_item.id, cron.id)
+
+
+if __name__ == '__main__':
+    main()
